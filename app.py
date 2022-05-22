ec4f59c3d09a37d96cea203a301f4a28509f838a
NEMO <NEMO@mac.local>
2022-05-22 21:16:11 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(name = None):
+    return render_template('index.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
