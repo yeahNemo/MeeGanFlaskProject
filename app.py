55a517edb48ffd56c05755b05e44056105ba9afc
NEMO <974025642@qq.com>
2022-05-24 07:02:50 +0800

    文创产品页面图片更新

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world(name = None):
-    return render_template('index.html', name=name)
+
+    return render_template('index.html')
+
+
+@app.route('/chart')
+def chartShow():
+    return render_template('TestHTML.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
